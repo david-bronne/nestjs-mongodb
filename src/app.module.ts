8c19d350fec0139085560f4ae6c92500a1187b3e
David Bronne <davidbronne@gmail.com>
2024-10-22 07:35:38 +0200

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
+// if authentication : MongooseModule.forRoot('mongodb://name:password@127.0.0.1/nestjs_api'
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1/nestjs_api'),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
