b69ff7e02727a0d62503f85b917190215239d7ef
David Bronne <davidbronne@gmail.com>
2024-10-22 23:44:39 +0200

    add update and delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, HttpException, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, Param, Patch, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/createUser.dto";
 import mongoose from "mongoose";
+import { UpdateUserDto } from "./dto/updateUser.dto";
 
 @Controller('users')
 export class UsersController {
@@ -28,5 +29,27 @@ export class UsersController {
         const findUser = await this.usersService.getUserById(id);
         if(!findUser) throw new HttpException('User not found', 404);
         return findUser
+    }
+
+    @Patch(':id')
+    async updateUserById(@Param('id') id:string, @Body() updateUserDto: UpdateUserDto){
+        // To be checked in a middelware
+        const isValid: boolean = mongoose.Types.ObjectId.isValid(id);
+        if(!isValid) throw new HttpException('User not found', 404);
+
+        const updateUser = await this.usersService.updateUserById(id, updateUserDto);
+        if(!updateUser) throw new HttpException('User not found', 404);
+        return updateUser;
+    }
+
+    @Delete(':id')
+    async deleteUserById(@Param('id') id:string){
+        // To be checked in a middelware
+        const isValid: boolean = mongoose.Types.ObjectId.isValid(id);
+        if(!isValid) throw new HttpException('User not found', 404);
+
+        const deleteUser = await this.usersService.deleteUserById(id);
+        if(!deleteUser) throw new HttpException('User not found', 404);
+        return;
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/User.shema";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { UpdateUserDto } from "./dto/updateUser.dto";
 
 @Injectable()
 export class UsersService{
@@ -21,5 +22,11 @@ export class UsersService{
         return this.userModel.findById(id);
     }
 
-    
+    updateUserById(id: string, updateUserDto: UpdateUserDto) {
+        return this.userModel.findByIdAndUpdate(id, updateUserDto, {new: true});
+    }
+
+    deleteUserById(id:string){
+        return this.userModel.findByIdAndDelete(id);
+    }
 }
